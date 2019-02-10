118bc4ad767c71860d7a308148cc948e132c79e5
Unknown <ljoseph@mail.uoguelph.ca>
2019-02-10 08:52:09 -0500

    Working

--- lib/home.dart
+++ lib/home.dart
@@ -7,28 +7,92 @@ class HomePage extends StatelessWidget {
       // appBar: new AppBar(
       //   title: new Text("TOP"),
       // ),
-      body: new  Column(
-        
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Container(
-              color: Colors.green,
-              child: Text(
-                "TOP",
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontFamily: "calibri"
+      body: new Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Padding(
+            padding: EdgeInsets.all(10.0),
+          ),
+          new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // color: Colors.green,
+              children: <Widget>[
+                Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "TOP Left",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
                 ),
-              ),
-            )
-          ],
-
-        ),
-      
+                 Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "TOP Center",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+                 Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "TOP Right",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+              ]),
+              new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // color: Colors.green,
+              children: <Widget>[
+                Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "MID Left",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+                 Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "MID Center",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+                 Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "MID Right",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+              ]),
+              new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // color: Colors.green,
+              children: <Widget>[
+                Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "Bottom Left",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+                 Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "Bottom Center",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+                 Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "Bottom Right",
+                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                  ),
+                ),
+              ])
+        ],
+      ),
     );
   }
 }
