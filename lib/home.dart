5c5b0c6edcc2c05475a578eaf9ceb78cb4542fc7
Unknown <ljoseph@mail.uoguelph.ca>
2019-02-13 13:23:57 -0500

    working

--- lib/home.dart
+++ lib/home.dart
@@ -3,96 +3,90 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      // appBar: new AppBar(
-      //   title: new Text("TOP"),
-      // ),
-      body: new Column(
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          new Padding(
-            padding: EdgeInsets.all(10.0),
+
+        // appBar: new AppBar(
+        //   title: new Text("TOP"),
+        // ),
+        body: Column(
+      children: <Widget>[
+        Text(
+          "Title Widget here",
+          style: new TextStyle(color: Colors.black),
+        ),
+        Padding(
+          padding: EdgeInsets.all(screenHeight * 0.15),
+        ),
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: screenWidth,
+            maxWidth: screenWidth,
+            minHeight: screenHeight * 0.5,
+            maxHeight: screenHeight * 0.5,
           ),
-          new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              // color: Colors.green,
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            color: Colors.amber,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "TOP Left",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                Expanded(
+                    child: new Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Expanded(
+                          child: RaisedButton(
+                            child: Text("btn1"),
+                            onPressed: () {},  
+                          ),
+                        ),
+                        Expanded(
+                          child: RaisedButton(
+                            child: Text("btn2"),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Expanded(
+                            child: RaisedButton(
+                            child: Text("btn3"),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("btn2"),
+                    onPressed: () {},
                   ),
                 ),
-                 Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "TOP Center",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("btn3"),
+                    onPressed: () {},
                   ),
-                ),
-                 Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "TOP Right",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-              ]),
-              new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              // color: Colors.green,
-              children: <Widget>[
-                Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "MID Left",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-                 Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "MID Center",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-                 Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "MID Right",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-              ]),
-              new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              // color: Colors.green,
-              children: <Widget>[
-                Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "Bottom Left",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-                 Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "Bottom Center",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-                 Container(
-                  color: Colors.amber,
-                  child: Text(
-                    "Bottom Right",
-                    style: TextStyle(fontSize: 20.0, fontFamily: "calibri"),
-                  ),
-                ),
-              ])
-        ],
-      ),
-    );
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
+
+// ConstrainedBox(
+//         constraints: BoxConstraints(
+//           minWidth: screenWidth,
+//           maxWidth: screenWidth,
+//           minHeight: screenHeight*0.5,
+//           maxHeight: screenHeight*0.5,
+//         ),
+//       child: Container(
+//         color: Colors.amber,
+//       ),
+//     )
