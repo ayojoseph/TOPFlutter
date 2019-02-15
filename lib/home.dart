322f20cfd7964140c0907f2b284bcdaa1f3f0573
Unknown <ljoseph@mail.uoguelph.ca>
2019-02-15 11:18:33 -0500

    More button work

--- lib/home.dart
+++ lib/home.dart
@@ -7,134 +7,144 @@ class HomePage extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-
         appBar: new AppBar(
           title: new Text("TOP"),
         ),
-        body: Column(
-      children: <Widget>[
-        Text(
-          "Title Widget here",
-          style: new TextStyle(
-              color: Colors.black,
-              fontSize: 30.0
+        body: Container(
+          decoration: new BoxDecoration(
+            image: DecorationImage(
+              image: ExactAssetImage('assets/knee.jpg'),
+              fit: BoxFit.cover
             ),
-        ),
-        Padding(
-          padding: EdgeInsets.all(screenHeight * 0.15),
-        ),
-        //Menu Box
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            minWidth: screenWidth,
-            maxWidth: screenWidth,
-            minHeight: screenHeight * 0.5,
-            maxHeight: screenHeight * 0.5,
           ),
-          child: Container(
-            padding: EdgeInsets.all(10.0),
-            color: Colors.amber,
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Expanded(
-                    child: new Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Expanded(
-                          child: FlatButton(
+            child: Column(
+            children: <Widget>[
+              Text(
+                "Title Widget here",
+                style: new TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+              Padding(
+                padding: EdgeInsets.all(screenHeight * 0.15),
+              ),
+              //Menu Box
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: screenWidth,
+                  maxWidth: screenWidth,
+                  minHeight: screenHeight * 0.5,
+                  maxHeight: screenHeight * 0.5,
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  // color: Colors.amber,
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Expanded(
+                          child: new Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Expanded(
+                              child: new Container(
+                                decoration: new BoxDecoration(
+                                  // borderRadius: BorderRadius.circular(1.0),
+                                  shape: BoxShape.circle,
+                                  color: Colors.lime
+                                ),
+                                child: FlatButton(
+                                  // padding: EdgeInsets.all(1.0),
                             onPressed: () {},
                             child: Column(
+
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
                                 // Icon(
                                 //   Icons.ac_unit,
                                 //   size: 40.0,
                                 // ),
-                                Image.asset('assets/donate.png'),
+                                Image.asset('assets/donate.png', fit: BoxFit.scaleDown,),
                                 Text("FlatButton")
                               ],
                             ),
-                          )
-                        ),
-                        Expanded(
-                          child: RaisedButton(
-
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Image(image: AssetImage('assets/internet.png'),),
-                              Text("IconButton")
-                              ],
+                            )
+                              ),
+                          ),
+                          Expanded(
+                            child: RaisedButton(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Image(
+                                    image: AssetImage('assets/internet.png'),
+                                  ),
+                                  Text("IconButton")
+                                ],
+                              ),
+                              onPressed: () {},
                             ),
-                            onPressed: () {},
                           ),
-                        ),
-                        Expanded(
+                          Expanded(
                             child: RaisedButton(
-                            child: Text("btn3"),
-                            onPressed: () {},
+                              child: Text("btn3"),
+                              onPressed: () {},
+                            ),
                           ),
-                        ),
-                      ],
-                    )
+                        ],
+                      )),
+                      Expanded(
+                          child: new Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Expanded(
+                            child: RaisedButton(
+                              child: Text("btn4"),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Expanded(
+                            child: RaisedButton(
+                              child: Text("btn5"),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Expanded(
+                            child: RaisedButton(
+                              child: Text("btn6"),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      )),
+                      Expanded(
+                          child: new Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Expanded(
+                            child: RaisedButton(
+                              child: Text("btn7"),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Expanded(
+                            child: RaisedButton(
+                              child: Text("btn8"),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Expanded(
+                            child: RaisedButton(
+                              child: Text("btn9"),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ))
+                    ],
+                  ),
                 ),
-                Expanded(
-                  child: new Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Expanded(
-                          child: RaisedButton(
-                            child: Text("btn4"),
-                            onPressed: () {},  
-                          ),
-                        ),
-                        Expanded(
-                          child: RaisedButton(
-                            child: Text("btn5"),
-                            onPressed: () {},
-                          ),
-                        ),
-                        Expanded(
-                            child: RaisedButton(
-                            child: Text("btn6"),
-                            onPressed: () {},
-                          ),
-                        ),
-                      ],
-                    )
-                ),
-                Expanded(
-                  child: new Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Expanded(
-                          child: RaisedButton(
-                            child: Text("btn7"),
-                            onPressed: () {},  
-                          ),
-                        ),
-                        Expanded(
-                          child: RaisedButton(
-                            child: Text("btn8"),
-                            onPressed: () {},
-                          ),
-                        ),
-                        Expanded(
-                            child: RaisedButton(
-                            child: Text("btn9"),
-                            onPressed: () {},
-                          ),
-                        ),
-                      ],
-                    )
-                )
-              ],
-            ),
-          ),
-        )
-      ],
-    ));
+              )
+            ],
+        )));
   }
 }
 
